4866171f88d9a504e2a760a64564ccce0187829e
Илья Шокин <mallet322@gmail.com>
2022-03-29 16:34:40 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/Store.java
+++ src/main/java/ru/job4j/di/Store.java
@@ -3,9 +3,11 @@ package ru.job4j.di;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Store {
 
     private List<String> data = new ArrayList<>();
